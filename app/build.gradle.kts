5bd75de57416f4c730ebe831e7cbd0bdb1f347c2
jayoadonis <jayo.adonisraphael@gmail.com>
2024-05-31 20:02:55 +0800

    chore: 0.0.1, added learn-java-algorithm/app

---
+++ app/build.gradle.kts
@@ -0,0 +1,17 @@
+
+plugins {
+    java
+    application
+}
+
+project.version = "0.0.1"
+project.group = "jayo.arb.learn_j"
+
+val ROOT_PROJECT_NAME = project.rootProject.name
+    .replace( Regex("[ .-]+"), "_" );
+
+project.application {
+    this.mainClass.set(
+        "${project.group}.${ROOT_PROJECT_NAME}.${project.name}.MainExe"
+    )
+}
---
+++ app/src/main/jayo/arb/learn_j/learn_java_algorithm/app/MainExe.java
@@ -0,0 +1,7 @@
+package jayo.arb.learn_j.learn_java_algorithm.app;
+
+public class MainExe {
+    public static void main(String[] args) {
+        System.out.println("Hi there from: learn-java-algorithm");
+    }
+}
